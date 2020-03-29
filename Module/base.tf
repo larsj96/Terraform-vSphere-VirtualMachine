69a55b918561891192b269f5af9f87d08c079c70
Jon Howe <howe.jon@gmail.com>
2020-03-29 11:27:37 -0400

    initial commit

---
+++ Module/base.tf
@@ -0,0 +1,32 @@
+# Base config
+# Vcenter connection parameters
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "Datacenter"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "vsanDatastore"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "Cluster"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "100-LabNetwork"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
---
+++ Module/main.tf
@@ -0,0 +1,48 @@
+resource "vsphere_virtual_machine" "vm" {
+  name             = var.vm_name
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = var.vcpu_count
+  memory   = var.memory
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  scsi_type = data.vsphere_virtual_machine.template.scsi_type
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      #https://www.terraform.io/docs/providers/vsphere/r/virtual_machine.html#linux-customization-options
+      linux_options {
+        host_name = var.vm_name
+        domain    = var.domain_name
+      }
+
+      network_interface {}
+
+      /*
+      network_interface {
+        ipv4_address = var.vm_ip
+        ipv4_netmask = var.vm_cidr
+    }
+      ipv4_gateway = var.default_gw
+      dns_server_list = ["1.2.3.4"]
+    */
+
+    }
+
+  }
+}
