a7c9c8a11e6eee24f734d215fa4b70e76a51c641
Jon Howe <howe.jon@gmail.com>
2020-04-01 11:55:38 -0400

    remvoed resource pool from output

--- output.tf
+++ output.tf
@@ -2,10 +2,6 @@ output "DC_ID" {
   description = "id of vSphere Datacenter"
   value       = data.vsphere_datacenter.dc.id
 }
-output "ResPool_ID" {
-  description = "Resource Pool id"
-  value       = data.vsphere_resource_pool.pool.id
-}
 
 output "Windows-VM" {
   description = "VM Names"
