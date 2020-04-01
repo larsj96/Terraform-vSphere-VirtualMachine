2beaf0bc4a8c91ebbcf129736299ab151e5c4a80
Jon Howe <howe.jon@gmail.com>
2020-04-01 13:16:02 -0400

    Moved data blocks to main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,35 @@
+# Vcenter connection parameters
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = var.datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.portgroup
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
 resource "vsphere_virtual_machine" "linux" {
   count             = var.is_windows_image ? 0 : 1
   name              = var.vm_name
