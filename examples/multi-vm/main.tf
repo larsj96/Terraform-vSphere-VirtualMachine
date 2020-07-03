e750df13a832bbf95f81d15f02037a7606488140
Jon Howe <howe.jon@gmail.com>
2020-07-03 13:39:41 -0400

    initial commit

---
+++ examples/multi-vm/main.tf
@@ -0,0 +1,61 @@
+module "DC1" {
+  is_windows_image = "1"
+  source         = "github.com/jonhowe/Terraform-vSphere-VirtualMachine/"
+  vsphere_server = "vcenter.home.lab"
+  vsphere_user = "administrator@vsphere.local"
+  vsphere_password = "VMware1!"
+  adminpassword = "terraform"
+  datacenter = "VirtJunkie-DC"
+  datastore = "vsanDatastore"
+  cluster = "VirtJunkie"
+  portgroup = "100-LabNetwork"
+  domain_name = "home.lab"
+  default_gw = "172.16.1.1"
+  template_name = "WS16-CORE"
+  vm_name = "DC1"
+  vm_ip = "172.16.1.150"
+  vm_cidr = 24
+  vcpu_count = 1
+  memory = 1024
+}
+module "MGMT" {
+  is_windows_image = "1"
+  source         = "github.com/jonhowe/Terraform-vSphere-VirtualMachine/"
+  vsphere_server = "vcenter.home.lab"
+  vsphere_user = "administrator@vsphere.local"
+  vsphere_password = "VMware1!"
+  adminpassword = "terraform"
+  datacenter = "VirtJunkie-DC"
+  datastore = "vsanDatastore"
+  cluster = "VirtJunkie"
+  portgroup = "100-LabNetwork"
+  domain_name = "home.lab"
+  default_gw = "172.16.1.1"
+  template_name = "WS16-GUI"
+  vm_name = "Mgmt"
+  vm_ip = "172.16.1.151"
+  vm_cidr = 24
+  vcpu_count = 2
+  memory = 4096
+}
+
+module "IAAS" {
+  is_windows_image = "1"
+  source         = "github.com/jonhowe/Terraform-vSphere-VirtualMachine/"
+  vsphere_server = "vcenter.home.lab"
+  vsphere_user = "administrator@vsphere.local"
+  vsphere_password = "VMware1!"
+  adminpassword = "terraform"
+  datacenter = "VirtJunkie-DC"
+  datastore = "vsanDatastore"
+  cluster = "VirtJunkie"
+  portgroup = "100-LabNetwork"
+  domain_name = "home.lab"
+  default_gw = "172.16.1.1"
+  template_name = "WS16-GUI"
+  vm_name = "IAAS"
+  vm_ip = "172.16.1.159"
+  vm_cidr = 24
+  vcpu_count = 2
+  memory = 4096
+}
